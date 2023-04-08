511d5147393a43347078b29702a38afd8a0f06fa
Pierce Chong <48197758+PierceCCH@users.noreply.github.com>
2023-04-07 17:46:29 -0700

    separate vision component

--- static/components/Vision/index.js
+++ static/components/Vision/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import { VisionContainer, VisionHeader, VisionHeaderWrapper, VisionSubHeader, VisionTopLine, VisionWrapper } from './VisonElements'
+import { 
+  VisionContainer, 
+  VisionHeaderWrapper, 
+  VisionHeadline, 
+  VisionSubtitle, 
+  VisionWrapper, 
+  Headline, 
+  VisionCenteredBox, 
+  VisionLink 
+} from './VisionElements'
 
 const Vision = () => {
   return (
     <VisionContainer>
-        <VisionWrapper>
-            <VisionHeaderWrapper>
-                <VisionTopLine> Our Vision </VisionTopLine>
-                <VisionHeader> To foster a global community for Singaporean culture within UCLA </VisionHeader>
-                <VisionSubHeader> we are a welcoming community for people of all backgrounds, introducing the Singaporean culture to the broader student body at UCLA </VisionSubHeader>
-            </VisionHeaderWrapper>
-        </VisionWrapper>
+      
     </VisionContainer>
   )
 }
