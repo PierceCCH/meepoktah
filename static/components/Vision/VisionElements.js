bd5df0f7090a9f60cb82dcd2deb7cb1de3a7d06c
Pierce Chong <48197758+PierceCCH@users.noreply.github.com>
2023-04-07 19:34:31 -0700

    redesigned

--- static/components/Vision/VisionElements.js
+++ static/components/Vision/VisionElements.js
@@ -2,106 +2,48 @@ import styled from 'styled-components'
 
 export const VisionContainer = styled.div`
     color: #fff;
-    background: ${({lightBg}) => (lightBg ? '#E8E9EB' : '#0b0e24')};
-    height: 1000px;
+    background: #AAAAAA;
+    height: fit-content;
     
     @media screen and (max-width: 768px){
         padding-top: 100px;
     }
 `
 
-export const VisionWrapper = styled.div`
-    display: grid;
-    z-index: 1;
-    height: 860px;
+export const VisionHeaderWrapper = styled.div`
+    display: flex;
+    background: linear-gradient(90deg, rgba(14,18,45,1) 0%, rgba(11,14,36,1) 50%, rgba(14,18,45,1) 100%);
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 340px;
     width: 100%;
     margin-right: auto;
     margin-left: auto;
-    justify-content: center;
-`
-
-export const VisionRow = styled.div`
-    display: grid;
-    grid-auto-columns: minmax(auto, 1fr);
-    align-items: center;
-    margin-top: 24px;
-    grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
-
-    @media screen and (max-width: 768px){
-        grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`)};
-    }
-`
-
-export const Column1 = styled.div`
-    margin-bottom: 15px;
-    grid-area: col1;
-    padding: 0 24px;
-`
-
-export const Column2 = styled.div`
-    margin-bottom: 15px;
-    grid-area: col2;
-`
-
-export const TextWrapper = styled.div`
-    max-width: 740px;
-    padding-top: 0;
-    padding-bottom: 60px;
-    margin-left: ${({imgStart}) => (imgStart ? '0px' : '150px')};
-
-    @media screen and (max-width: 768px){
-        padding-bottom: 0px;
-        margin-left: 0px;
-    }
-`
-
-export const TopLine = styled.p`
-    color: ${({lightText}) => (lightText ? '#E4B363' : '#E60000')};
-    font-size: 16px;
-    line-height: 16px;
-    font-weight: 700;
-    letter-spacing: 1.4px;
-    text-transform: uppercase;
-    margin-bottom: 16px;
-
-    @media screen and (max-width: 480px){
-        font-size: 14px;
-    }
 `
 
 export const Heading = styled.h1`
     margin-bottom: 24px;
-    font-size: 48px;
+    font-size: 132px;
     font-weight: 600;
-    color: ${({lightText}) => (lightText ? '#f7f8fa' : '#1D3461')};
+    color: #FFFFFF;
+    font-family: 'Montserrat', sans-serif;
 
     @media screen and (max-width: 480px){
         font-size: 32px;
     }
 `
 
-export const Subtitle = styled.p`
-    margin-bottom: 24px;
-    font-size: 1.5rem;
-    line-height: 2rem;
-    color: ${({darkText}) => (darkText ? '#000' : '#fff')};
-
-    @media screen and (max-width: 480px){
-        line-height: 1.2rem;
-        font-size: 1.2rem;
-    }
+export const VisionWrapper = styled.div`
+    height: 800px;
 `
 
-export const BtnWrap = styled.div`
-    display: flex;
-    justify-content: flex-start;
-`
-
-export const ImgWrap = styled.div`
+export const ImgBg = styled.img`
+    width: 100%;
     height: 100%;
-    max-width: 800px;
-    margin: 16px;
+    object-fit: cover;
     transition: all 0.1s ease-in-out;
+    filter: blur(1.2px) grayscale(0.3) hue-rotate(-30deg);
 
     @media screen and (max-width: 768px){
         max-width: 440px;
@@ -110,11 +52,42 @@ export const ImgWrap = styled.div`
     }
 `
 
-export const Img = styled.img`
+export const ImgTextWrapper = styled.div`
+    position: absolute;
+    padding-left: 8px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, 520%);
+    color: #fff;
+    text-align: left;
     width: 100%;
-    border-radius: 5px;
-` 
+    
+    z-index: 2;
+`
 
-export const MarginDiv = styled.div`
-    margin-top: ${props => (props.margin || '-60px')};
+export const ImgText = styled.h1`
+    font-size: 96px;
+    font-weight: 600;
+    color: #fff;
+    font-family: 'Montserrat', sans-serif;
+    max-height: 100%;
+    
+    text-shadow: 5px 2px 10px rgba(0,0,0,0.6);
+
+    @media screen and (max-width: 480px){
+        font-size: 32px;
+    }
+`
+
+export const TopLine = styled.p`
+    color: #FFF;
+    font-size: 48px;
+    line-height: 16px;
+    font-weight: 700;
+    margin-bottom: 24px;
+
+    text-decoration: gold underline;
+    text-underline-offset: 12px;
+
+    text-shadow: 5px 2px 10px rgba(0,0,0,0.6);
 `
--- static/components/Vision/index.js
+++ static/components/Vision/index.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import { 
   VisionContainer, 
-  VisionHeaderWrapper, 
-  VisionHeadline, 
-  VisionSubtitle, 
+  VisionHeaderWrapper,
+  Heading,
   VisionWrapper, 
-  Headline, 
-  VisionCenteredBox, 
-  VisionLink 
+  ImgTextWrapper,
+  ImgText,
+  ImgBg,
+  TopLine
 } from './VisionElements'
+import img1 from "/static/images/infoSection1.jpeg"
 
 const Vision = () => {
+
+  const img1String = "CREATE AN INCLUSIVE SINGAPOREAN COMMUNITY AT UCLA"
   return (
     <VisionContainer>
-      
+      <VisionHeaderWrapper>
+        <Heading> A HOME AWAY FROM HOME </Heading>
+      </VisionHeaderWrapper>
+      <VisionWrapper>
+        <ImgTextWrapper>
+          <TopLine> OUR VISION </TopLine>
+          <ImgText> {img1String} </ImgText>
+        </ImgTextWrapper>
+        <ImgBg src={img1} alt="Vision" />
+      </VisionWrapper>
     </VisionContainer>
   )
 }
